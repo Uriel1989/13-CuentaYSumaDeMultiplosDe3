6283b0033334ff9134da26190f5ed040f54564be
Uriel Guillermo Daniel <45216219+Uriel1989@users.noreply.github.com>
2022-06-15 20:28:43 -0300

    Add files via upload

---
+++ 13-CuentaYSumaDeMultiplosDe3/13-CuentaYSumaDeMultiplosDe3/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace _13_CuentaYSumaDeMultiplosDe3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i, a, b;
+            a = 0;
+            b = 0;
+            for (i = 0; i <= 100; i++)
+            {
+
+                if (i % 3 == 0)
+                {
+                    Console.WriteLine("Los multiplos de 3 son " + i);
+                    b = b + i;
+                }
+
+            }
+            Console.WriteLine("La suma total da: " + b);
+
+            Console.ReadKey();
+        }
+    }
+}
